09003d1b89ca5ca0350433bed69cfb3dbef67845
WhiteCmile <54742594+WhiteCmile@users.noreply.github.com>
2023-12-08 10:56:01 +0800

    Update evaluation.cpp
    add remarks

--- src/evaluation.cpp
+++ src/evaluation.cpp
@@ -10,68 +10,68 @@
 extern std :: map<std :: string, ExprType> primitives;
 extern std :: map<std :: string, ExprType> reserved_words;
 
-Value Let::eval(Assoc &env) {}
+Value Let::eval(Assoc &env) {} // let expression
 
-Value Lambda::eval(Assoc &env) {}
+Value Lambda::eval(Assoc &env) {} // lambda expression
 
-Value Apply::eval(Assoc &e) {}
+Value Apply::eval(Assoc &e) {} // for function calling
 
-Value Letrec::eval(Assoc &env) {}
+Value Letrec::eval(Assoc &env) {} // letrec expression
 
-Value Var::eval(Assoc &e) {}
+Value Var::eval(Assoc &e) {} // evaluation of variable
 
-Value Fixnum::eval(Assoc &e) {}
+Value Fixnum::eval(Assoc &e) {} // evaluation of a fixnum
 
-Value If::eval(Assoc &e) {}
+Value If::eval(Assoc &e) {} // if expression
 
-Value True::eval(Assoc &e) {}
+Value True::eval(Assoc &e) {} // evaluation of #t
 
-Value False::eval(Assoc &e) {}
+Value False::eval(Assoc &e) {} // evaluation of #f
 
-Value Begin::eval(Assoc &e) {}
+Value Begin::eval(Assoc &e) {} // begin expression
 
-Value Quote::eval(Assoc &e) {}
+Value Quote::eval(Assoc &e) {} // quote expression
 
-Value MakeVoid::eval(Assoc &e) {}
+Value MakeVoid::eval(Assoc &e) {} // (void)
 
-Value Exit::eval(Assoc &e) {}
+Value Exit::eval(Assoc &e) {} // (exit)
 
-Value Binary::eval(Assoc &e) {}
+Value Binary::eval(Assoc &e) {} // evaluation of two-operators primitive
 
-Value Unary::eval(Assoc &e) {}
+Value Unary::eval(Assoc &e) {} // evaluation of single-operator primitive
 
-Value Mult::evalRator(const Value &rand1, const Value &rand2) {}
+Value Mult::evalRator(const Value &rand1, const Value &rand2) {} // *
 
-Value Plus::evalRator(const Value &rand1, const Value &rand2) {}
+Value Plus::evalRator(const Value &rand1, const Value &rand2) {} // +
 
-Value Minus::evalRator(const Value &rand1, const Value &rand2) {}
+Value Minus::evalRator(const Value &rand1, const Value &rand2) {} // -
 
-Value Less::evalRator(const Value &rand1, const Value &rand2) {}
+Value Less::evalRator(const Value &rand1, const Value &rand2) {} // <
 
-Value LessEq::evalRator(const Value &rand1, const Value &rand2) {}
+Value LessEq::evalRator(const Value &rand1, const Value &rand2) {} // <=
 
-Value Equal::evalRator(const Value &rand1, const Value &rand2) {}
+Value Equal::evalRator(const Value &rand1, const Value &rand2) {} // =
 
-Value GreaterEq::evalRator(const Value &rand1, const Value &rand2) {}
+Value GreaterEq::evalRator(const Value &rand1, const Value &rand2) {} // >=
 
-Value Greater::evalRator(const Value &rand1, const Value &rand2) {}
+Value Greater::evalRator(const Value &rand1, const Value &rand2) {} // >
 
-Value IsEq::evalRator(const Value &rand1, const Value &rand2) {}
+Value IsEq::evalRator(const Value &rand1, const Value &rand2) {} // eq?
 
-Value Cons::evalRator(const Value &rand1, const Value &rand2) {}
+Value Cons::evalRator(const Value &rand1, const Value &rand2) {} // cons
 
-Value IsBoolean::evalRator(const Value &rand) {}
+Value IsBoolean::evalRator(const Value &rand) {} // boolean?
 
-Value IsFixnum::evalRator(const Value &rand) {}
+Value IsFixnum::evalRator(const Value &rand) {} // fixnum?
 
-Value IsNull::evalRator(const Value &rand) {}
+Value IsNull::evalRator(const Value &rand) {} // null?
 
-Value IsPair::evalRator(const Value &rand) {}
+Value IsPair::evalRator(const Value &rand) {} // pair?
 
-Value IsProcedure::evalRator(const Value &rand) {}
+Value IsProcedure::evalRator(const Value &rand) {} // procedure?
 
-Value Not::evalRator(const Value &rand) {}
+Value Not::evalRator(const Value &rand) {} // not
 
-Value Car::evalRator(const Value &rand) {}
+Value Car::evalRator(const Value &rand) {} // car
 
-Value Cdr::evalRator(const Value &rand) {}
+Value Cdr::evalRator(const Value &rand) {} // cdr
