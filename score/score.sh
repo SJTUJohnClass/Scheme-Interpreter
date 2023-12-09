f9543697b8b673acbcb459dc7521f45e92d56267
WhiteCmile <whitecmile@WhiteCmile>
2023-12-09 13:30:44 +0800

    move more-tests, update score.sh

--- score/score.sh
+++ score/score.sh
@@ -28,3 +28,29 @@ EOF
     echo "---------------------------"
     echo ""
 done
+
+L_EXTRA=1
+R_EXTRA=7
+for ((i = $L_EXTRA; i <= $R_EXTRA; i = i + 1))
+do
+    echo ""
+    echo "---------------------------"
+    echo "Ready to test: EXTRA TEST" $i
+    ../bin/myscheme << EOF > scm.out
+    $(cat ./more-tests/$i.in)
+    (exit)
+EOF
+    sed '$d' scm.out > scm_cleaned.out
+    mv scm_cleaned.out scm.out
+    sed 's/scm> //' scm.out > scm_cleaned.out
+    mv scm_cleaned.out scm.out
+    diff scm.out ./more-tests/$i.out > diff_output.txt
+    if [ $? -ne 0 ]; then
+        echo "Wrong answer in EXTRA TEST" $i
+        # echo "---------------------------"
+        # echo ""
+        # exit 1
+    fi
+    echo "---------------------------"
+    echo ""
+done
